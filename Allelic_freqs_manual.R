f689c7e3c6c733309b71025f3d429493c58796f9
Hernan E. Morales <hern.moral@gmail.com>
2017-03-03 11:51:07 -0800

    Update Allelic_freqs_manual.R

--- Allelic_freqs_manual.R
+++ Allelic_freqs_manual.R
@@ -29,34 +29,34 @@ setwd("./")
 ##################################
 # Read input parameters and data #
 ##################################
-# Read in the 012 table, the whole table and first row for name of samples
+# Table in 012 format with headers with marker columns and sample names
 # The table has n columns with individual names and n rows with marker names
-x<- fread("SNP.TABLE.txt", header=T, sep=",")
-x<- data.frame(x)
-# fread complains if individual names start with number (it adds a "X."), if this is the case read the first row of the file again (header) and ssign it as names
-x.1<- fread("../../SNP_Tables/SNP_Table_DArT_hetero_error.012.transectSAMPLES.csv", header=F, sep=",",nrows = 1)
-names(x)<- x.1
+SNPtable<- fread("SNP.TABLE.txt", header=T, sep=",", data.table=F)
+# fread adds a prefix "X" to header names that start with number, if this is the case read the first row of the file again (header) and assign back to column names
+# a work-around this problem is using "read.table" with check.names=FALSE, but would be very slow for large tables
+x.1<- fread("SNP.TABLE.txt", header=F, sep=",",nrows = 1)
+names(SNPtable)<- x.1
 
-# Read info file with samples populations, with same individual names as "x" (SNP.TABLE.txt)
-z<- read.table("FILE.txt")
+# Population file: text file with each individual assigned to one population. Individual names should be the same as in "SNPtable"
+POPtable<- read.table("POP.FILE.txt")
 
 # Make vector containing genotypes only (i.e. remove extra columns, i.e. Indv names, mapping information, etc) by subsetting data
-geno<- x[,12:ncol(x)] # Change "12" with the number of column where genotypes start in "x" (SNP.TABLE.txt)
-# Add column with the name of the loci
-row.names(geno)<- x$SNP_Code
+geno<- SNPtable[,N:ncol(SNPtable)] # Change "N" with the number of column where genotypes start in SNPtable
+# Add column with the name of the loci from SNPtable
+row.names(geno)<- SNPtable$SNP_Code
 
 # Dummy set with 1000 loci for test code
 #geno<- geno[1:1000,]
 
 ## assign all non {0,1,2} to NA
-geno[(geno=="NA")] <- NA
+geno[(geno=="NA")] <- NA # Or whatever character for missing data e.g. -9
 # Transpose the geno object
 geno<- data.frame(t(geno))
 # Assign a new column  with the name of the individuals
 geno$Code<- row.names(geno)
 # merge the sample info table ABOVE with the geno object to assign populations to individuals
-geno<- merge(geno,z[c("Code", "POP")], by = "Code")
-# remove first column of geno (names)
+geno<- merge(geno,POPtable[c("Code", "POP")], by = "Code")
+# remove first column of geno (Indv names)
 geno<- geno[,-1]
 # Make a vector with list of pops
 POPS<- names(table(geno$POP))
